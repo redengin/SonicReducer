be039231a66922f7cfe3a1810952152f25a73f10
redengin <sholstei@yahoo.com>
2026-01-19 22:19:59 -0500

    in work

--- esp32/main/modulator.c
+++ esp32/main/modulator.c
@@ -1,7 +1,7 @@
 #include "modulator.h"
 
 #include <soc/io_mux_reg.h>
-#include <driver/i2s.h>
+#include <driver/gpio.h>
 
 const size_t CARRIER_HZ = 40 * 1000;    // 40 KHz
 
@@ -14,6 +14,9 @@ void modulator_init(void)
     // NOT USED gpio_set_drive_capability(GPIO_NUM_0, GPIO_DRIVE_CAP_3);
 }
 
+#define USE_I2S_LEGACY
+#ifdef USE_I2S_LEGACY
+#include <driver/i2s.h>
 void modulator_config(const modulator_config_t* const config)
 {
     const i2s_config_t cfg = {
@@ -41,3 +44,26 @@ void modulator_start() {
 void modulator_stop() {
     ESP_ERROR_CHECK(i2s_stop(I2S_NUM_0));
 }
+#else
+#include <driver/i2s_pdm.h>
+
+static i2s_chan_handle_t modulator_chan_handle;
+void modulator_config(const modulator_config_t* const config)
+{
+    // create an I2S channel
+    const i2s_chan_config_t tx_chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
+    ESP_ERROR_CHECK(i2s_new_channel(&tx_chan_cfg, &modulator_chan_handle, NULL));
+
+    // configure the channel for trasmit
+    const i2s_pdm_tx_config_t pdm_tx_cfg = {
+        .clk_cfg = I2S_PDM_TX_CLK_DEFAULT_CONFIG(CARRIER_HZ),
+        /* The data bit-width of PDM mode is fixed to 16 */
+        .slot_cfg = I2S_PDM_TX_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
+    };
+    ESP_ERROR_CHECK(i2s_channel_init_pdm_tx_mode(modulator_chan_handle, &pdm_tx_cfg));
+
+    // enable the tx channel
+    ESP_ERROR_CHECK(i2s_channel_enable(modulator_chan_handle));
+}
+
+#endif // USE_I2S_LEGACY
