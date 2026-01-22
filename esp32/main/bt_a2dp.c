5a93b9a3c6046c2e206c9cab24a4d10cb665038e
redengin <sholstei@yahoo.com>
2026-01-21 21:31:44 -0500

    in work

--- esp32/main/bt_a2dp.c
+++ esp32/main/bt_a2dp.c
@@ -5,8 +5,18 @@
 #include <esp_bt_main.h>
 #include <esp_bt_device.h>
 #include <esp_gap_bt_api.h>
+#include <esp_avrc_api.h>
+#include <esp_a2dp_api.h>
 
 static const char *LOG_TAG = "bt-a2dp";
+
+// forward declarations
+static void bt_app_dev_cb(esp_bt_dev_cb_event_t event, esp_bt_dev_cb_param_t *param);
+static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param);
+static void bt_app_rc_ct_cb(esp_avrc_ct_cb_event_t event, esp_avrc_ct_cb_param_t *param);
+static void bt_app_rc_tg_cb(esp_avrc_tg_cb_event_t event, esp_avrc_tg_cb_param_t *param);
+static void bt_app_a2d_cb(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *param);
+static void bt_app_a2d_audio_data_cb(esp_a2d_conn_hdl_t conn_hdl, esp_a2d_audio_buff_t *audio_buf);
 
 void bt_a2dp_init(
     const char *const device_name, ///< bluetooth published name
@@ -38,44 +48,71 @@ void bt_a2dp_init(
             ESP_BT_PIN_TYPE_FIXED, SIZEOF_PIN_CODE, _pin_code));
     }
 
-//     esp_bt_gap_set_device_name(local_device_name);
-//     esp_bt_dev_register_callback(bt_app_dev_cb);
-//     esp_bt_gap_register_callback(bt_app_gap_cb);
+    // set the published bluetooth nane
+    ESP_ERROR_CHECK(esp_bt_gap_set_device_name(device_name));
 
-//     esp_avrc_ct_register_callback(bt_app_rc_ct_cb);
-//     assert(esp_avrc_ct_init() == ESP_OK);
-//     esp_avrc_tg_register_callback(bt_app_rc_tg_cb);
-//     assert(esp_avrc_tg_init() == ESP_OK);
+    // register callbacks
+    ESP_ERROR_CHECK(esp_bt_dev_register_callback(bt_app_dev_cb));
+    ESP_ERROR_CHECK(esp_bt_gap_register_callback(bt_app_gap_cb));
+    ESP_ERROR_CHECK(esp_avrc_ct_register_callback(bt_app_rc_ct_cb));
+    ESP_ERROR_CHECK(esp_avrc_ct_init());
+    ESP_ERROR_CHECK(esp_avrc_tg_register_callback(bt_app_rc_tg_cb));
+    ESP_ERROR_CHECK(esp_avrc_tg_init());
+    esp_avrc_rn_evt_cap_mask_t evt_set = {0};
+    assert(esp_avrc_rn_evt_bit_mask_operation(ESP_AVRC_BIT_MASK_OP_SET, &evt_set, ESP_AVRC_RN_VOLUME_CHANGE));
+    ESP_ERROR_CHECK(esp_avrc_tg_set_rn_evt_cap(&evt_set));
+    ESP_ERROR_CHECK(esp_a2d_register_callback(&bt_app_a2d_cb));
+    ESP_ERROR_CHECK(esp_a2d_sink_init());
 
-//     esp_avrc_rn_evt_cap_mask_t evt_set = {0};
-//     esp_avrc_rn_evt_bit_mask_operation(ESP_AVRC_BIT_MASK_OP_SET, &evt_set, ESP_AVRC_RN_VOLUME_CHANGE);
-//     assert(esp_avrc_tg_set_rn_evt_cap(&evt_set) == ESP_OK);
+    // configure the A2DP support - ESP32 only supports mSBC currently
+    esp_a2d_mcc_t mcc = {0};
+    mcc.type = ESP_A2D_MCT_SBC;
+    mcc.cie.sbc_info.samp_freq = 0xf;
+    mcc.cie.sbc_info.ch_mode = 0xf;
+    mcc.cie.sbc_info.block_len = 0xf;
+    mcc.cie.sbc_info.num_subbands = 0x3;
+    mcc.cie.sbc_info.alloc_mthd = 0x3;
+    mcc.cie.sbc_info.max_bitpool = 250;
+    mcc.cie.sbc_info.min_bitpool = 2;
+    ESP_ERROR_CHECK(esp_a2d_sink_register_stream_endpoint(0, &mcc));
+    ESP_ERROR_CHECK(esp_a2d_sink_register_audio_data_callback(bt_app_a2d_audio_data_cb));
 
-//     esp_a2d_register_callback(&bt_app_a2d_cb);
-//     assert(esp_a2d_sink_init() == ESP_OK);
+    // FIXME what do these do?
+    // ESP_ERROR_CHECK(esp_a2d_sink_get_delay_value());
+    // ESP_ERROR_CHECK(esp_bt_gap_get_device_name());
 
-// #if CONFIG_EXAMPLE_A2DP_SINK_USE_EXTERNAL_CODEC == FALSE
-//     esp_a2d_sink_register_data_callback(bt_app_a2d_data_cb);
-// #else
-//     esp_a2d_mcc_t mcc = {0};
-//     mcc.type = ESP_A2D_MCT_SBC;
-//     mcc.cie.sbc_info.samp_freq = 0xf;
-//     mcc.cie.sbc_info.ch_mode = 0xf;
-//     mcc.cie.sbc_info.block_len = 0xf;
-//     mcc.cie.sbc_info.num_subbands = 0x3;
-//     mcc.cie.sbc_info.alloc_mthd = 0x3;
-//     mcc.cie.sbc_info.max_bitpool = 250;
-//     mcc.cie.sbc_info.min_bitpool = 2;
-//     /* register stream end point, only support mSBC currently */
-//     esp_a2d_sink_register_stream_endpoint(0, &mcc);
-//     esp_a2d_sink_register_audio_data_callback(bt_app_a2d_audio_data_cb);
-// #endif
-//     /* Get the default value of the delay value */
-//     esp_a2d_sink_get_delay_value();
-//     /* Get local device name */
-//     esp_bt_gap_get_device_name();
+    // set discoverable and connectable mode, wait to be connected
+    ESP_ERROR_CHECK(esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE));
 
-//     /* set discoverable and connectable mode, wait to be connected */
-//     esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+    ESP_LOGI(LOG_TAG, "initialized");
+}
+
+static void bt_app_dev_cb(esp_bt_dev_cb_event_t event, esp_bt_dev_cb_param_t *param)
+{
+    // TODO implement
+}
+
+static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+    // TODO implement
+}
+
+static void bt_app_rc_ct_cb(esp_avrc_ct_cb_event_t event, esp_avrc_ct_cb_param_t *param)
+{
+    // TODO implement
+}
+
+static void bt_app_rc_tg_cb(esp_avrc_tg_cb_event_t event, esp_avrc_tg_cb_param_t *param)
+{
+    // TODO implement
+}
+
+static void bt_app_a2d_cb(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *param)
+{
+    // TODO implement
+}
+
+static void bt_app_a2d_audio_data_cb(esp_a2d_conn_hdl_t conn_hdl, esp_a2d_audio_buff_t *audio_buf)
+{
 
 }
